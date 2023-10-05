8059928e73ac218018428ae6d6804863d29c0769
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-05 22:12:19 +0200

    02. Works

--- 02. grades.py
+++ 02. grades.py
@@ -1,1 +1,16 @@
 grade = float(input())
+def exam_solve(grade_score):
+    if 2.00 <= grade_score <= 2.99:
+         return "Fail"
+    elif 3.00 <= grade_score <= 3.49: 
+        return "Poor"
+    elif 3.50 <= grade_score <= 4.49:
+        return "Good"
+    elif 4.50 <= grade_score <= 5.49:
+        return "Very Good"
+    elif 5.50 <= grade_score <= 6.00:
+        return "Excellent"
+    else:
+        return "No score"
+    
+print(exam_solve(grade))    
