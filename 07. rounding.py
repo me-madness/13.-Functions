cd2de42a1329be23bf49a9836d35f80fb08a8757
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-06 22:38:48 +0200

    06. use eval function

--- 07. rounding.py
+++ 07. rounding.py
@@ -1,15 +1,5 @@
 rounding = input().split()
-new_list = []
+new_list = [eval(i) for i in rounding]
+# new_list = map(round, rounding)     
+print(round(new_list))
 
-for i in rounding:
-    new_list.append(i)
-print(new_list)
-
-
-
-# def rounding(num):
-#     for i in range(rounding):
-#         counter_new_list = input()
-#         counter_new_list.append(new_list)
-#         new_list = int(round(new_list))
-# print(rounding(new_list))        
