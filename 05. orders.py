175723bcb61426faa625d209ae0851dd86473e13
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-06 21:32:33 +0200

    05 begining

---
+++ 05. orders.py
@@ -0,0 +1,19 @@
+product = input()
+quantity = int(input())
+
+def orders(product, quantity):
+    if product == "coffee":
+        return 1.50 * quantity
+    elif product == "water":
+        return 1.00 * quantity
+    elif product == "coke":
+        return 1.40 * quantity
+    elif product == "snacks":
+        return 2.00 * quantity
+    else:
+                        
+        
+        
+        
+        
+   
