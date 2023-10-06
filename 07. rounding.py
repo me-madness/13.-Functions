d1edc256cb92ed57c867b6052c11d90e79bd757b
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-06 21:52:28 +0200

    all most done

---
+++ 07. rounding.py
@@ -0,0 +1,8 @@
+rounding = input().split()
+new_list = []
+def rounding(num):
+    for i in range(rounding):
+        counter_new_list = input()
+        new_list.append(counter_new_list)
+        new_list = int(round(new_list))
+print(rounding(new_list))        
