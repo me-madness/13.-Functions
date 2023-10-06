9b46db3edb3a1e3d1fd42e0baae21373958d10fb
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-06 22:03:05 +0200

    append

--- 07. rounding.py
+++ 07. rounding.py
@@ -1,8 +1,15 @@
 rounding = input().split()
 new_list = []
-def rounding(num):
-    for i in range(rounding):
-        counter_new_list = input()
-        new_list.append(counter_new_list)
-        new_list = int(round(new_list))
-print(rounding(new_list))        
+
+for i in rounding:
+    new_list.append(i)
+print(new_list)
+
+
+
+# def rounding(num):
+#     for i in range(rounding):
+#         counter_new_list = input()
+#         counter_new_list.append(new_list)
+#         new_list = int(round(new_list))
+# print(rounding(new_list))        
