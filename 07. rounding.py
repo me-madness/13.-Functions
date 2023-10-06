8c36a66b9be91733434a61b54d1dcac48d19400c
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-06 22:44:28 +0200

    06 finish

--- 07. rounding.py
+++ 07. rounding.py
@@ -1,5 +1,6 @@
 rounding = input().split()
 new_list = [eval(i) for i in rounding]
-# new_list = map(round, rounding)     
-print(round(new_list))
+map_new_list = map(round, new_list)     
+print(list(map_new_list))
+# print(new_list)
 
